77303a93a8f73d6c49263757ffc42a6a67ac6d98
Peter Butkovic <butkovic@gmail.com>
2025-03-14 14:06:38 +0100

    feat: initial fyne based impl

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
-	"github.com/getlantern/systray"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
 )
 
 type TempoDiPomo struct {
@@ -14,59 +19,68 @@ type TempoDiPomo struct {
 	paused        bool
 	timeLeft      time.Duration
 	isWorkSession bool
+	app           fyne.App
+	window        fyne.Window
+	timeLabel     *widget.Label
+	startButton   *widget.Button // Declare buttons as struct fields
+	pauseButton   *widget.Button
 }
 
 func main() {
-	systray.Run(onReady, onExit)
-}
-
-func onReady() {
+	fyneApp := app.New()
 	p := &TempoDiPomo{
 		workDuration:  25 * time.Minute,
 		breakDuration: 5 * time.Minute,
 		timeLeft:      25 * time.Minute,
 		isWorkSession: true,
+		app:           fyneApp,
 	}
+	p.window = fyneApp.NewWindow("TempoDiPomo")
+	p.run()
+}
 
-	systray.SetTitle("🍅 25:00")
-	systray.SetTooltip("TempoDiPomo - Pomodoro Timer")
+func (p *TempoDiPomo) run() {
+	p.timeLabel = widget.NewLabel("🍅 Work 25:00")
 
-	mStart := systray.AddMenuItem("Start", "Start the timer")
-	mPause := systray.AddMenuItem("Pause", "Pause/Resume the timer")
-	mReset := systray.AddMenuItem("Reset", "Reset the timer")
-	systray.AddSeparator()
-	mQuit := systray.AddMenuItem("Quit", "Quit the app")
-
-	mPause.Disable()
-
-	go func() {
-		for {
-			select {
-			case <-mStart.ClickedCh:
-				if !p.running {
-					p.running = true
-					mStart.Disable()
-					mPause.Enable()
-					go p.runTimer()
-				}
-			case <-mPause.ClickedCh:
-				p.paused = !p.paused
-				if p.paused {
-					mPause.SetTitle("Resume")
-				} else {
-					mPause.SetTitle("Pause")
-				}
-			case <-mReset.ClickedCh:
-				p.reset()
-				mStart.Enable()
-				mPause.Disable()
-				mPause.SetTitle("Pause")
-			case <-mQuit.ClickedCh:
-				systray.Quit()
-				return
-			}
+	p.startButton = widget.NewButton("Start", func() {
+		if !p.running {
+			p.running = true
+			p.startButton.Disable()
+			p.pauseButton.Enable()
+			go p.runTimer()
 		}
-	}()
+	})
+	p.pauseButton = widget.NewButton("Pause", func() {
+		p.paused = !p.paused
+		if p.paused {
+			p.pauseButton.SetText("Resume")
+		} else {
+			p.pauseButton.SetText("Pause")
+		}
+	})
+	p.pauseButton.Disable()
+	resetButton := widget.NewButton("Reset", func() {
+		p.reset()
+		p.startButton.Enable()
+		p.pauseButton.Disable()
+		p.pauseButton.SetText("Pause")
+	})
+	configButton := widget.NewButton("Configure", func() {
+		p.showConfigDialog()
+	})
+	quitButton := widget.NewButton("Quit", func() {
+		p.app.Quit()
+	})
+
+	content := container.NewVBox(
+		p.timeLabel,
+		container.NewHBox(p.startButton, p.pauseButton, resetButton),
+		configButton,
+		quitButton,
+	)
+
+	p.window.SetContent(content)
+	p.window.ShowAndRun()
 }
 
 func (p *TempoDiPomo) runTimer() {
@@ -93,21 +107,24 @@ func (p *TempoDiPomo) updateDisplay() {
 	if !p.isWorkSession {
 		session = "Break"
 	}
-	systray.SetTitle(fmt.Sprintf("🍅 %s %02d:%02d", session, minutes, seconds))
+	p.timeLabel.SetText(fmt.Sprintf("🍅 %s %02d:%02d", session, minutes, seconds))
 }
 
 func (p *TempoDiPomo) sessionComplete() {
 	p.running = false
 	if p.isWorkSession {
-		notify("Work session complete!", "Time for a break!")
+		p.notify("Work session complete!", "Time for a break!")
 		p.timeLeft = p.breakDuration
 		p.isWorkSession = false
 	} else {
-		notify("Break session complete!", "Time to work!")
+		p.notify("Break session complete!", "Time to work!")
 		p.timeLeft = p.workDuration
 		p.isWorkSession = true
 	}
 	p.updateDisplay()
+	p.startButton.Enable()
+	p.pauseButton.Disable()
+	p.pauseButton.SetText("Pause")
 }
 
 func (p *TempoDiPomo) reset() {
@@ -118,10 +135,37 @@ func (p *TempoDiPomo) reset() {
 	p.updateDisplay()
 }
 
-func notify(title, message string) {
-	fmt.Printf("Notification: %s - %s\n", title, message)
+func (p *TempoDiPomo) notify(title, message string) {
+	dialog.NewInformation(title, message, p.window).Show()
 }
 
-func onExit() {
-	fmt.Println("TempoDiPomo app exited")
+func (p *TempoDiPomo) showConfigDialog() {
+	workEntry := widget.NewEntry()
+	workEntry.SetText(strconv.Itoa(int(p.workDuration.Minutes())))
+	breakEntry := widget.NewEntry()
+	breakEntry.SetText(strconv.Itoa(int(p.breakDuration.Minutes())))
+
+	form := dialog.NewForm("Configure Timer", "Save", "Cancel", []*widget.FormItem{
+		{Text: "Work Duration (5-120 min):", Widget: workEntry},
+		{Text: "Break Duration (1-60 min):", Widget: breakEntry},
+	}, func(confirmed bool) {
+		if confirmed {
+			workMinutes, workErr := strconv.Atoi(workEntry.Text)
+			breakMinutes, breakErr := strconv.Atoi(breakEntry.Text)
+			if workErr == nil && breakErr == nil && workMinutes >= 5 && workMinutes <= 120 && breakMinutes >= 1 && breakMinutes <= 60 {
+				p.workDuration = time.Duration(workMinutes) * time.Minute
+				p.breakDuration = time.Duration(breakMinutes) * time.Minute
+				if p.isWorkSession {
+					p.timeLeft = p.workDuration
+				} else {
+					p.timeLeft = p.breakDuration
+				}
+				p.updateDisplay()
+			} else {
+				p.notify("Invalid Input", "Work: 5-120 min, Break: 1-60 min")
+			}
+		}
+	}, p.window)
+
+	form.Show()
 }
